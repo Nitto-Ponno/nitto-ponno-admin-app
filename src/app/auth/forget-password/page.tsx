d0dc685d584cbe4635f8a7586dd28788414a48a1
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2025-10-30 23:04:58 +0600

    auth setup (#8)
    * auth setup
    
    * fix isuue

---
+++ src/app/auth/forget-password/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ForgetPasswordForm from '@/components/pages/auth/forget-password/forget-password-form';
+
+const ForgetPassword = () => {
+  return (
+    <div className="mx-auto w-lg">
+      <ForgetPasswordForm />
+    </div>
+  );
+};
+
+export default ForgetPassword;
