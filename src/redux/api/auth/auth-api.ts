15b44860911df60fc737ceb116203028224c1650
Sagor Ahamed <sagorahamed251245@gmail.com>
2025-11-03 21:35:21 +0600

    signup done

--- src/redux/api/auth/auth-api.ts
+++ src/redux/api/auth/auth-api.ts
@@ -1,3 +1,4 @@
+import { TSignupUser } from '@/types';
 import { baseApi } from '../baseApi';
 
 const authAPi = baseApi.injectEndpoints({
@@ -6,6 +7,23 @@ const authAPi = baseApi.injectEndpoints({
       query: (data) => ({
         overrideExisting: true,
         url: '/auth/admin-login',
+        method: 'POST',
+        data: data,
+        headers: {},
+      }),
+    }),
+    signupUser: build.mutation({
+      query: (data: TSignupUser) => ({
+        overrideExisting: true,
+        url: '/auth/customer-register',
+        method: 'POST',
+        data: data,
+        headers: {},
+      }),
+    }),
+    forgetPassword: build.mutation({
+      query: (data) => ({
+        url: '/auth/forgot-password',
         method: 'POST',
         data: data,
         headers: {},
@@ -21,4 +39,8 @@ const authAPi = baseApi.injectEndpoints({
   overrideExisting: false,
 });
 
-export const { useLoginUserMutation } = authAPi;
+export const {
+  useLoginUserMutation,
+  useForgetPasswordMutation,
+  useSignupUserMutation,
+} = authAPi;
--- src/types/auth/auth-types.ts
+++ src/types/auth/auth-types.ts
@@ -2,3 +2,14 @@ export type TUser = {
   name: string;
   _id: string;
 };
+
+export type TSignupUser = {
+  email: string;
+  password: string;
+  phoneNumber: string;
+  name: {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  };
+};
